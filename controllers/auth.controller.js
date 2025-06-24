892ee570f689e81691baacb2bdfe3ebee4e33625
prathamverma77 <prathamverma1980@gmail.com>
2025-06-25 00:08:11 +0530

    feat: implement user registration and login functionality with JWT authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,84 @@
-export const registerUser = (req, res) => {
-    res.send('Register route');
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
+
+
+
+//REGISTER LOGIC
+export const registerUser = async (req, res) => {
+    try {
+        const {name, email, password, role}= req.body;
+
+        //checking if user already exists
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message:'Email already exists'});
+        }
+
+        //hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //create user
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            role,
+        });
+
+        //send token
+        const token = generateToken(newUser._id);
+
+        res.status(201).json({
+            success: true,
+            user: {
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.role,
+            },
+            token,
+        });
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({message: 'server error'});
+    }
 };
 
-export const loginUser = (req, res) => {
-    res.send('Login route');
 
+//LOGIN LOGIC
+export const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        //check if email exists in DB
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({message:'Invalid Credential'});
+        }
+
+        //compare plain password with hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:'Invalid Credentials'});
+        }
+
+        //Generate JWT token
+        const token = generateToken(user._id);
+
+        //send user data (excluding password)
+        res.status(200).json({
+            success: true,
+            user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+        });
+    }catch (error){
+        console.error(error);
+        res.status(500).json({message:'server error'});
+    }
 };
---
+++ middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    //1. check if token is present in Authorization header
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            //Extract token (Bearer tokenvalue)
+            token = req.headers.authorization.split('')[1];
+            //verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            //Fetch user from DB, exclude password
+            req.user = await User.findById(decoded.id).select('-password');
+            //pass control to next middleware/route
+            next();
+        }catch (error){
+            console.error('Auth error:', error.message);
+            return res.status(401).json({message:'Not authorized, invalid token'});
+        }
+    }
+    //if no token
+    if (!token){
+        return res.status(401).json({message:'Not authorized,no token'});
+    }
+}
